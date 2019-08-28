f84490dcdcb67d4ca47e215102dac62ee6e6843c
mashaborodina <borodina061721@gmail.com>
2019-08-28 12:20:14 +0300

    finalized module_09

--- js/module_09/notepad-boilerplate-homework-09/src/js/app.js
+++ js/module_09/notepad-boilerplate-homework-09/src/js/app.js
@@ -215,9 +215,60 @@ const generateUniqueId = () =>
     .toString(36)
     .substring(2, 15);
 
-// REFERRALS
+//-------------------------------Ссылки на узел DOM--------------------------------//
+
 const refs = {
-  form: document.querySelector('.note-editor'),
-  noteList: document.querySelector('.note-list'),
-  searchForm: document.querySelector('form.search-form'),
+  form: document.querySelector('.note-editor'), //новая заметка
+  noteList: document.querySelector('.note-list'), //ul
+  search: document.querySelector('form.search-form'), //фильтрация заметок
 }
+
+//----------------------------------Слушатели---------------------------------------//
+refs.form.addEventListener('submit', handleSubmitForm);
+refs.noteList.addEventListener('click', removeListItem);
+refs.search.addEventListener('input', filterNotes);
+
+//---------------------------------Обработчики--------------------------------------//
+
+// форма отправки
+function addListItem(listRef, note) {
+  listRef.append(createListItem(note));
+}
+
+function handleSubmitForm(event) {
+  event.preventDefault();
+  let [title, body] = document.querySelectorAll('.note-editor__input');
+  if (!title.value || !body.value) {
+    return alert(`Необходимо заполнить все поля!`);
+  }
+
+// добавление новой заметки
+  let addNewNote = notepad.saveNote({
+    id: generateUniqueId(),
+    title: title.value,
+    body: body.value,
+    priority: 0,
+  });
+  addListItem(refs.noteList, addNewNote);
+  event.target.reset();
+}
+
+// удаление заметки
+function removeListItem(event) {
+  if (event.target.closest('.action').dataset.action === 'delete-note') {
+    notepad.deleteNote(event.target.closest('.note-list__item').dataset.id);
+    event.target.closest('.note-list__item').remove();
+  } else {
+    return;
+  }
+}
+
+// фильтрация заметки
+function filterNotes(event) {
+  refs.noteList.innerHTML = '';
+  renderNoteList(refs.noteList, notepad.filterNotesByQuery(event.target.value));
+}
+
+
+
+
